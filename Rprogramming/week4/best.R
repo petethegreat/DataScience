c39766ead05c8d90e0354076a63d263e82c97e8a
Peter Thompson <peter.thompson@radimetrics.com>
2017-01-10 20:38:45 -0500

    best seems to work

--- Rprogramming/week4/best.R
+++ Rprogramming/week4/best.R
@@ -5,15 +5,35 @@ best<-function(state,outcome)
 	## Check that state and outcome are valid
 	## Return hospital name in that state with lowest 30-day death
 	## rate
+	
+	#check outcome is sensible
 	outcomes<-c("heart attack"=11,"heart failure"=17,"pneumonia"=23)
 	if ( ! outcome  %in% names(outcomes) )
 	{
 		stop('invalid outcome')
 	}
+	
+	# read the data
 	data<-read.csv('outcome-of-care-measures.csv',na.strings='Not Available',stringsAsFactors=FALSE)
-	# outcome<-read.csv('outcome-of-care-measures.csv',na.strings='Not Available',stringsAsFactors=FALSE)
+	
+	
+	# check state is sensible
 	if (! (state %in% data$State) )
 	{
 		stop('invalid state')
 	}
+	
+	# c(2,7,outcome) => name,state,outcome
+	# slimmed contains only the three columns of interest (state is of interest?) ,
+	# and only the rows corresponding to the state of interest
+	slimmed<-data[ data$State==state, c(2,7,outcomes[outcome])]
+	
+	# sorted the slimmed dataframe/slice. order by outcome first, then name
+	sorted<-order(slimmed[,3],slimmed[,1],na.last=TRUE)
+	
+	return(slimmed[sorted[1],1])
+	
+	
+	
+	
 }
